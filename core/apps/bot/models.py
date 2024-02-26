d5dc498b1cb2611fc8b16a2801842bf3faef2b73
zxc.idol <ViperX_YT@mail.ru>
2024-02-26 20:42:57 +0300

    Правки

--- core/apps/bot/models.py
+++ core/apps/bot/models.py
@@ -5,3 +5,11 @@ class pizza(models.Model):
     pizza_name = models.TextField()
     size = models.TextField()
     price = models.TextField()
+    ingredients = models.TextField()
+
+class orders(models.Model):
+    user_id = models.TextField()
+    pizza_name = models.TextField()
+    product_price = models.TextField()
+    id = models.AutoField(primary_key=True)
+    size = models.TextField()
